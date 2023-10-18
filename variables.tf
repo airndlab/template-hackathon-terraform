e7cd5d7e5e6c1927b034168dd53bf83c4d2e73e4
Grig Alex <alexengrigdev@gmail.com>
2023-10-18 12:24:22 +0300

    Add variables.tf

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "cloud_id" {
+  type        = string
+  description = "ID of cloud"
+  nullable    = false
+  sensitive   = true
+}
+
+variable "folder_id" {
+  type        = string
+  description = "ID of folder in cloud"
+  nullable    = false
+  sensitive   = true
+}
+
+variable "service_account_id" {
+  type        = string
+  description = "ID of service account in folder"
+  nullable    = false
+  sensitive   = true
+}
+
+variable "hack_name" {
+  type        = string
+  description = "Short name of hackathon"
+  nullable    = false
+  sensitive   = false
+}
