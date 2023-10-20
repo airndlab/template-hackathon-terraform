917c2f69eaa81944cf1a19d2ab89278fe0bdaec0
Grig Alex <alexengrigdev@gmail.com>
2023-10-20 13:28:26 +0300

    Fix serverless_containers.tf yet

--- serverless_containers.tf
+++ serverless_containers.tf
@@ -2,7 +2,7 @@ resource "yandex_serverless_container" "py_app" {
   service_account_id = var.service_account_id
   name               = "${var.hack_name}-py-app"
   memory             = 128
-  image              = {
+  image {
     url = "cr.yandex/${yandex_container_registry.this.id}/airndlab/template-hackathon-python-fastapi:0.1.0"
   }
 }
