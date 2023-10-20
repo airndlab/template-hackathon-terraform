ce5aac7cb957cb4089f401eac82aba13b24e4a77
Grig Alex <alexengrigdev@gmail.com>
2023-10-20 12:18:05 +0300

    Add terraform backend in yandex object storage

--- main.tf
+++ main.tf
@@ -5,6 +5,18 @@ terraform {
     }
   }
   required_version = ">= 0.13"
+
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    region     = "ru-central1"
+    key        = "${var.hack_name}/terraform.tfstate"
+    bucket     = var.object_storage_bucket
+    access_key = var.object_storage_access_key
+    secret_key = var.object_storage_secret_key
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
 }
 
 provider "yandex" {
