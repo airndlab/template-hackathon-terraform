a9cc054db9e6a1aa7e94ad59000fee3698f337ee
Grig Alex <alexengrigdev@gmail.com>
2023-11-17 18:42:59 +0300

    Update vm

--- vm.tf
+++ vm.tf
@@ -1,13 +1,26 @@
 data "yandex_compute_image" "container_optimized_image" {
+  # https://cloud.yandex.com/en-ru/marketplace/products/yc/container-optimized-image
   family = "container-optimized-image"
 }
 
 resource "yandex_compute_instance" "this" {
-  name = "${var.hack_name}-vm"
-  zone = "ru-central1-b"
+  # https://cloud.yandex.com/en/docs/compute/concepts/vm-platforms#standard-platforms
+  platform_id               = "standard-v3"
+  name                      = "${var.hack_name}-vm"
+  zone                      = "ru-central1-b"
+  allow_stopping_for_update = true
+
+  resources {
+    cores  = 10
+    memory = 10
+  }
 
   boot_disk {
     initialize_params {
+      # https://cloud.yandex.com/en/docs/compute/concepts/disk#disks-types
+      type     = "network-ssd"
+      name     = "${var.hack_name}-disk"
+      size     = 100
       image_id = data.yandex_compute_image.container_optimized_image.id
     }
   }
@@ -17,17 +30,8 @@ resource "yandex_compute_instance" "this" {
     nat       = true
   }
 
-  resources {
-    cores  = 2
-    memory = 2
-  }
-
   metadata = {
     docker-compose = file("${path.module}/docker-compose.yml")
-    user-data      = file("${path.module}/vm_config.yml")
+    user-data      = file("${path.module}/cloud_config.yaml")
   }
-}
-
-output "external_ip" {
-  value = yandex_compute_instance.this.network_interface.0.nat_ip_address
 }
